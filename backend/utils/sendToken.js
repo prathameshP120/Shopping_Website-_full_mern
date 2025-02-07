dc2ada491f0926fcde04a46d00940acd01f8eff1
prathameshP120 <156537026+prathameshP120@users.noreply.github.com>
2025-02-07 18:26:51 +0530

    Update sendToken.js

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -1,11 +1,4 @@
-/*let us create a function that will assign the token and then save that token in the cookie , 
-Now ,if we dont know , we can make an http only cookie , now http only cookie can only be acccessed
-from the server side , cannot be accessed on the client side (  httpOnly: true ), so you can say that it is a safe place to 
-put our token , Because definitely we have to save the token somewhere so that we can check before each request 
- that the user is authenticated or not . for that we have to save the token and best place to save the token is 
- goging to be http only cookie because that cannot be acessed from the front-end , it can only be accessed on the backend
 
-*/
 // Create token and save in the cookie
 export default (user, statusCode, res) => {
   // Create JWT Token
